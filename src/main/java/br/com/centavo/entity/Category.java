d01afaa46aeaf90ed296e235730b21b6be2a7e02
Geberson Antonio Ferreira Mendes <gebersonf@kyros.com.br>
2026-03-12 23:47:24 -0300

    fixing category entity

--- src/main/java/br/com/centavo/entity/Category.java
+++ src/main/java/br/com/centavo/entity/Category.java
@@ -1,62 +1,86 @@
-//package br.com.centavo.entity;
-//
-//import br.com.centavo.enums.BudgetType;
-//import br.com.centavo.enums.TransactionType;
-//
-//public class Category {
-//    private Long id;
-//    private Long userId;
-//    private String name;
-//    private TransactionType type;
-//    private BudgetType budgetType;
-//
-//    public Category(){}
-//
-//    public Category(Long id, Long userId, String name, TransactionType type, BudgetType budgetType) {
-//        this.id = id;
-//        this.userId = userId;
-//        this.name = name;
-//        this.type = type;
-//        this.budgetType = budgetType;
-//    }
-//
-//    public Long getId() {
-//        return id;
-//    }
-//
-//    public void setId(Long id) {
-//        this.id = id;
-//    }
-//
-//    public Long getUserId() {
-//        return userId;
-//    }
-//
-//    public void setUserId(Long userId) {
-//        this.userId = userId;
-//    }
-//
-//    public String getName() {
-//        return name;
-//    }
-//
-//    public void setName(String name) {
-//        this.name = name;
-//    }
-//
-//    public TransactionType getType() {
-//        return type;
-//    }
-//
-//    public void setType(TransactionType type) {
-//        this.type = type;
-//    }
-//
-//    public BudgetType getBudgetType() {
-//        return budgetType;
-//    }
-//
-//    public void setBudgetType(BudgetType budgetType) {
-//        this.budgetType = budgetType;
-//    }
-//}
+package br.com.centavo.entity;
+
+import br.com.centavo.enums.BudgetType;
+import br.com.centavo.enums.TransactionType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "categories")
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private TransactionType type;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private BudgetType budgetType;
+
+    public Category(){}
+
+    public Category(String name, TransactionType type, User user, BudgetType budgetType) {
+        this.name = name;
+        this.type = type;
+        this.user = user;
+        this.budgetType = budgetType;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public TransactionType getType() {
+        return type;
+    }
+
+    public void setType(TransactionType type) {
+        this.type = type;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public BudgetType getBudgetType() {
+        return budgetType;
+    }
+
+    public void setBudgetType(BudgetType budgetType) {
+        this.budgetType = budgetType;
+    }
+}
