1c0906c13c4d41ee6c62137e9fc596ab8c3f881b
Geberson Antonio Ferreira Mendes <gebersonf@kyros.com.br>
2026-03-19 23:10:21 -0300

    create Transaction.java and TransactionRepository

--- src/main/java/br/com/centavo/entity/Transaction.java
+++ src/main/java/br/com/centavo/entity/Transaction.java
@@ -1,84 +1,109 @@
-//package br.com.centavo.entity;
-//
-//import br.com.centavo.enums.TransactionType;
-//
-//import java.math.BigDecimal;
-//import java.time.LocalDate;
-//
-//public class Transaction {
-//    private Long id;
-//    private Long accountId;
-//    private Long categoryId;
-//    private LocalDate date;
-//    private BigDecimal value;
-//    private String description;
-//    private TransactionType type;
-//
-//    public Transaction(){}
-//
-//    public Transaction(Long id, Long accountId, Long categoryId, LocalDate date, BigDecimal value, String description, TransactionType type) {
-//        this.id = id;
-//        this.accountId = accountId;
-//        this.categoryId = categoryId;
-//        this.date = date;
-//        this.value = value;
-//        this.description = description;
-//        this.type = type;
-//    }
-//
-//    public Long getId() {
-//        return id;
-//    }
-//
-//    public void setId(Long id) {
-//        this.id = id;
-//    }
-//
-//    public Long getAccountId() {
-//        return accountId;
-//    }
-//
-//    public void setAccountId(Long accountId) {
-//        this.accountId = accountId;
-//    }
-//
-//    public Long getCategoryId() {
-//        return categoryId;
-//    }
-//
-//    public void setCategoryId(Long categoryId) {
-//        this.categoryId = categoryId;
-//    }
-//
-//    public LocalDate getDate() {
-//        return date;
-//    }
-//
-//    public void setDate(LocalDate date) {
-//        this.date = date;
-//    }
-//
-//    public BigDecimal getValue() {
-//        return value;
-//    }
-//
-//    public void setValue(BigDecimal value) {
-//        this.value = value;
-//    }
-//
-//    public String getDescription() {
-//        return description;
-//    }
-//
-//    public void setDescription(String description) {
-//        this.description = description;
-//    }
-//
-//    public TransactionType getType() {
-//        return type;
-//    }
-//
-//    public void setType(TransactionType type) {
-//        this.type = type;
-//    }
-//}
+package br.com.centavo.entity;
+
+import br.com.centavo.enums.TransactionType;
+import jakarta.persistence.Column;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+public class Transaction {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String description;
+
+    @Column(nullable = false, precision = 19, scale = 2)
+    private BigDecimal value;
+
+    @Column(nullable = false)
+    private LocalDate date;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "account_id", nullable = false)
+    private Account account;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Category category;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private TransactionType type;
+
+    public Transaction(){}
+
+    public Transaction(Long id, String description, BigDecimal value, LocalDate date, TransactionType type, Account account, Category category) {
+        this.id = id;
+        this.description = description;
+        this.value = value;
+        this.date = date;
+        this.type = type;
+        this.account = account;
+        this.category = category;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public BigDecimal getValue() {
+        return value;
+    }
+
+    public void setValue(BigDecimal value) {
+        this.value = value;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public Account getAccount() {
+        return account;
+    }
+
+    public void setAccount(Account account) {
+        this.account = account;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+
+    public TransactionType getType() {
+        return type;
+    }
+
+    public void setType(TransactionType type) {
+        this.type = type;
+    }
+}
