9dfd13799c86d7a6a493ff7a5b582ce3ab1a94b1
Geberson Antonio Ferreira Mendes <gebersonf@kyros.com.br>
2026-03-23 23:22:08 -0300

    adding transaction service with create

---
+++ src/main/java/br/com/centavo/controller/TransactionController.java
@@ -0,0 +1,28 @@
+package br.com.centavo.controller;
+
+import br.com.centavo.dto.TransactionRequest;
+import br.com.centavo.dto.TransactionResponse;
+import br.com.centavo.entity.Transaction;
+import br.com.centavo.service.TransactionService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/transactions")
+public class TransactionController {
+    private final TransactionService transactionService;
+
+    public TransactionController(TransactionService transactionService) {
+        this.transactionService = transactionService;
+    }
+
+    @PostMapping
+    public ResponseEntity<TransactionResponse> create(@RequestBody TransactionRequest request) {
+        TransactionResponse response = transactionService.createTransaction(request);
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    }
+}
--- src/main/java/br/com/centavo/entity/Transaction.java
+++ src/main/java/br/com/centavo/entity/Transaction.java
@@ -2,6 +2,7 @@ package br.com.centavo.entity;
 
 import br.com.centavo.enums.TransactionType;
 import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
 import jakarta.persistence.FetchType;
@@ -10,10 +11,13 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
+@Entity
+@Table(name = "transactions")
 public class Transaction {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
